ed9538e7d03a6c93f356ff1726442a0067e55ab3
Peto Gurin <no.mi83@yahoo.com.vn>
2023-05-10 13:29:35 +0700

    Fecth

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import MoviesList from './components/MoviesList';
-import './App.css';
+import MoviesList from "./components/MoviesList";
+import "./App.css";
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+  const [state, setState] = useState([]);
+  async function fecthHandler() {
+    const res = await fetch("https://swapi.dev/api/films/");
+    const data = await  res.json();
+    const movie = data.results.map((item) => {
+      return {
+        id: item.episode_id,
+        title: item.title,
+        openingText: item.opening_crawl,
+        releaseDate: item.release_date,
+      };
+    });
+    setState(movie);
+  }
 
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <button onClick={fecthHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        <MoviesList movies={state} />
       </section>
     </React.Fragment>
   );
